392fdae776c43f60116344d2edfbd5cae57944df
diamond19990628 <yaoyupeng009@gmail.com>
2026-05-22 01:55:02 +0900

    完成了图片轮播

--- src/index/index.tsx
+++ src/index/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import style from "./index.module.css";
+import Photography from "./photography";
 const menuList:{id:number, name:string}[] = [
-    {id:1, name:"摄影"},
-    {id:2, name:"阅读"},
-    {id:3, name:"探索"},
-    {id:4, name:"更多..."}
+    {id:0, name:"摄影"},
+    {id:1, name:"阅读"},
+    {id:2, name:"探索"},
+    {id:3, name:"更多..."}
 ];
 const Main:React.FC = () => {
     const [activeIndex, setActiveIndex] = useState(0);
@@ -12,13 +13,19 @@ const Main:React.FC = () => {
         setActiveIndex(index);
     }
     return (
-        <div className={style.main_header}>
-            <h1 className={style.title}>我的爱好</h1>
-            <h2 className={style.subtitle}>My interests</h2>
-            <ul className={style.ul}>
-                {menuList.map((item,index)=>(<li key={item.id} className={`${style.li} ${index === activeIndex ? style.mouseClick : ''}`} onClick={()=>handleMenuMove(index)}>{item.name}</li>))}
-            </ul>
+        <div className={style.main}>
+            <div className={style.main_header}>
+                <h1 className={style.title}>我的爱好</h1>
+                <h2 className={style.subtitle}>My interests</h2>
+                <ul className={style.ul}>
+                    {menuList.map((item,index)=>(<li key={item.id} className={`${style.li} ${index === activeIndex ? style.mouseClick : ''}`} onClick={()=>handleMenuMove(index)}>{item.name}</li>))}
+                </ul>
+            </div>
+            <div className={style.main_body}>
+                {activeIndex === 0 && <Photography />}
+            </div>
         </div>
+        
     );
 }
 export default Main;
--- src/index/photography.tsx
+++ src/index/photography.tsx
@@ -1,6 +1,44 @@
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Photography1 from "../assets/photography1.png";
+import Photography2 from "../assets/photography2.png";
+import Photography3 from "../assets/photography3.png";
+import Photography4 from "../assets/photography4.png";
+import PhotoGraphy5 from "../assets/photography5.png";
+import Photography6 from "../assets/photography6.png";
+import style from "./photography.module.css";
+const imgList = [
+    {id:1, src:Photography1},
+    {id:2, src:Photography2},
+    {id:3, src:Photography3},
+    {id:4, src:Photography4},
+    {id:5, src:PhotoGraphy5},
+    {id:6, src:Photography6}
+]
 const photograph: React.FC = () => {
     return (
-        <div></div>
+        <>
+            <h1 className={style.main_body_title}>摄影</h1>
+            <Swiper
+                modules={[Autoplay]}
+                slidesPerView="auto"
+                spaceBetween={2}
+                loop={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                className={style.swiper}
+                >
+                {imgList.map((img) => (
+                    <SwiperSlide key={img.id}>
+                    <img src={img.src} alt={`Photography ${img.id}`} className={style.swiper_img}/>
+                    </SwiperSlide>
+                ))}
+                </Swiper>
+        </>
+        
     )
 }
 export default photograph;
